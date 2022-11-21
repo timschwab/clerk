819d92e8a0adbca52cb13bf8ece934fb814be7d3
Tim Schwab <timothy.john.schwab@gmail.com>
2022-11-21 10:02:10 -0500

    remove unneeded client-side budget creation

--- src/api/budget.js
+++ src/api/budget.js
@@ -4,11 +4,6 @@ async function fromGroup(group) {
 	return await network.get("/budget/fromGroup/" + group);
 }
 
-// Theoretically this should never have to be used
-async function create(group) {
-	return await network.post("/budget/create/" + group);
-}
-
 async function info(budgetId) {
 	return await network.get("/budget/" + budgetId);
 }
@@ -19,7 +14,6 @@ async function saveRevenue(budgetId, revenue) {
 
 export default {
 	fromGroup,
-	create,
 	info,
 	saveRevenue
 };
--- src/vue/GroupBudget.vue
+++ src/vue/GroupBudget.vue
@@ -3,20 +3,10 @@
 		<router-link :to="backLink">Back to group</router-link>
 
 		<div v-if="loaded">
-			<div v-if="successful">
-				<h1>Budget</h1>
-				<p>
-					It looks like this group doesn't have a budget yet. Click below to
-					create one!
-				</p>
-				<p><button @click="create">Create budget</button></p>
-			</div>
-			<div v-else>
-				<p>
-					There was some sort of problem loading this group's budget. Yeah, not
-					a very helpful error message, I know.
-				</p>
-			</div>
+			<p>
+				There was some sort of problem loading this group's budget. Yeah, not a
+				very helpful error message, I know.
+			</p>
 		</div>
 		<div v-else>
 			<p>Loading...</p>
@@ -50,27 +40,11 @@ export default {
 		async fetchBudget() {
 			let budget = await budgetApi.fromGroup(this.group);
 
-			if (budget.success) {
-				if (budget.return) {
-					this.gotoBudget(budget.return);
-				} else {
-					// Stay on the page and have the user create a budget
-					this.successful = true;
-				}
-			} else {
-				this.successful = false;
+			if (budget.success && budget.return) {
+				this.gotoBudget(budget.return);
 			}
 
 			this.loaded = true;
-		},
-		async create() {
-			let budget = await budgetApi.create(this.group);
-
-			if (budget.success) {
-				this.gotoBudget(budget.return);
-			} else {
-				this.toastStore.error(budget.message);
-			}
 		},
 		gotoBudget(budget) {
 			this.$router.push({
