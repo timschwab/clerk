360334e9e1184a8bb5640c2247839c44f14741aa
Tim Schwab <timothy.john.schwab@gmail.com>
2022-11-21 16:17:34 -0500

    show stats on the budget homepage

--- src/vue/BudgetHome.vue
+++ src/vue/BudgetHome.vue
@@ -8,6 +8,12 @@
 			<h1>Budget</h1>
 			<p><router-link :to="revenueLink">Revenue</router-link></p>
 			<p><router-link :to="expensesLink">Expenses</router-link></p>
+
+			<p>Revenue: {{ revenueTotal.string }}</p>
+			<p>Expenses: {{ expensesTotal.string }}</p>
+			<p>Savings: Coming soon...</p>
+			<p>Spending: {{ spendingMoneyString }}</p>
+			<p>Padding: {{ padding }}</p>
 		</div>
 		<div v-else>
 			<p>Loading...</p>
@@ -18,6 +24,7 @@
 <script>
 import useToast from "./stores/toast";
 import budgetApi from "../api/budget";
+import utils from "../utils";
 
 export default {
 	props: ["budget"],
@@ -41,6 +48,40 @@ export default {
 		},
 		expensesLink() {
 			return "/budget/" + this.budget + "/expenses";
+		},
+		revenueTotal() {
+			let val = Object.values(this.data.revenue).reduce(
+				(acc, cur) => acc + cur,
+				0
+			);
+			return {
+				number: val,
+				string: utils.numToDollar(val)
+			};
+		},
+		expensesTotal() {
+			let val = 0;
+			console.log(this.data.expenses);
+			for (let clump of Object.values(this.data.expenses)) {
+				for (let expense of Object.values(clump)) {
+					val += expense;
+				}
+			}
+
+			return {
+				number: val,
+				string: utils.numToDollar(val)
+			};
+		},
+		spendingMoneyString() {
+			return utils.numToDollar(this.data.spendingMoney);
+		},
+		padding() {
+			let val =
+				this.revenueTotal.number -
+				this.expensesTotal.number -
+				this.data.spendingMoney;
+			return utils.numToDollar(val);
 		}
 	},
 	methods: {
